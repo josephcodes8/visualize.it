ca707f0999e5c4792115b2848668f7de67978597
josephdevelops <josephapps8@gmail.com>
2024-02-14 19:30:37 +0300

    render bars according to the generated array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,27 @@ const display = (element, style = "block") => (element.style.display = style)
 const hide = (element, style = "none") => (element.style.display = style)
 const select = (selector) => document.querySelector(selector)
 
+// by default this function generates array with 10 items ranging from 1 upto 100
+const getRandomNumbers = function (n = 10, min = 1, max = 100) {
+  const arr = []
+
+  for (let i = 0; i < n; i++) {
+    arr.push(Math.trunc(Math.random() * max) + min)
+  }
+  return arr
+}
+
+const getBars = (arr, max) => {
+  const bars = arr.map((el) => {
+    const height = (el * 400) / max
+    return `<div class="bar" style="height:${height.toFixed(
+      2
+    )}px"><span>${el}</span></div>`
+  })
+  console.log(bars)
+  return bars.toString()
+}
+
 // DOM Elements
 const containerEl = select("#container")
 const buttonEl = select("#button")
@@ -14,18 +35,31 @@ const sizeEl = select("#array-size")
 const mainEl = select("#main")
 const sectionEl = select("#section")
 const loadingPage = select("#loading-page")
+const arrayEl = select("#array-element")
 
 // Event Listeners
 buttonEl.addEventListener("click", function () {
+  const arr = getRandomNumbers(20, 1, 1000)
+
+  const max = arr.reduce((a, b) => Math.max(a, b), -Infinity)
+  const maxTenth = Math.ceil(max / 10) * 10
+  select("#value--max").textContent = maxTenth
+  select("#value--middle").textContent = maxTenth / 2
+
   display(loadingPage)
+
   setTimeout(() => {
     select(".loading-page__heading").textContent = "Ready"
+
     setTimeout(() => {
       hide(mainEl)
       hide(loadingPage)
       display(sectionEl)
+      arrayEl.textContent = `[${arr.toString()}]( size = ${arr.length} )`
     }, 500)
   }, 1000)
+
+  select("#bars").insertAdjacentHTML("afterbegin", getBars(arr, maxTenth))
 })
 
 const init = function () {
