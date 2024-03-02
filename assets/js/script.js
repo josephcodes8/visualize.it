ae16ee395fab40f20e9105e8494c64423a18fb9b
josephdevelops <josephapps8@gmail.com>
2024-03-02 10:44:05 +0300

    add a dynamic sorting algorithm calling funcitonality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@
 // Utility functions
 const display = (element, style = "block") => (element.style.display = style)
 const hide = (element, style = "none") => (element.style.display = style)
+
 const select = (selector) => document.querySelector(selector)
 
-// by default this function generates array with 10 items ranging from 1 upto 100
 const getRandomNumbers = function (n = 10, min = 1, max = 100) {
+  /* 
+    Descirption: returns array of size `n` ranging between `min` and `max`
+    
+    @param {number} n      = number of  generated numbers @default = 10 
+    @param {number} min    = the minmum number to be generated @default = 1
+    @param {number} max    = the maximum number to be generated @default = 100 
+
+    @returns [array]       = array of `n` number between `min` and `max`   
+  */
+
   const arr = []
 
   for (let i = 0; i < n; i++) {
@@ -38,10 +48,17 @@ const arrayEl = select("#array-element")
 
 // Event Listeners
 buttonEl.addEventListener("click", function () {
-  const arr = getRandomNumbers(10, 50, 2150)
+  const arraySize = Number(sizeEl.value) // get the array size to be generated
+  const selectedAlgorithm = select(".input--radio:checked").value // get the selected sorting algorithm
+  const sortingSpeedInMS = speedEl.value * 1000 // get the sorting speed in milli seconds
 
+  const arr = getRandomNumbers(arraySize, 1, 10000)
+
+  // calcualte the next multiple of 10 of the maximum number generated in the array
   const max = arr.reduce((a, b) => Math.max(a, b), -Infinity)
   const maxTenth = Math.ceil(max / 10) * 10
+
+  // set the `middle-number` and `max-number` in the bar
   select("#value--max").textContent = maxTenth
   select("#value--middle").textContent = maxTenth / 2
 
@@ -49,17 +66,29 @@ buttonEl.addEventListener("click", function () {
 
   setTimeout(() => {
     select(".loading-page__heading").textContent = "Ready"
+    select(
+      "#algorithm-name"
+    ).textContent = `${selectedAlgorithm[0].toUpperCase()}${selectedAlgorithm
+      .slice(1)
+      .toLowerCase()} Sort`
 
     setTimeout(() => {
-      hide(mainEl)
-      hide(loadingPage)
-      display(sectionEl)
-      arrayEl.textContent = `[${arr.join(", ")}]( size = ${arr.length} )`
+      showInitalArray(`[${arr.join(", ")}]( size = ${arr.length} )`)
+      setTimeout(() => {
+        window[selectedAlgorithm + "Sort"](sortingSpeedInMS) // call the selected algorithm dynamically
+      }, 1000)
     }, 500)
   }, 1000)
 
   select("#bars").insertAdjacentHTML("afterbegin", getBars(arr, maxTenth))
 })
+
+const showInitalArray = function (array) {
+  hide(mainEl)
+  hide(loadingPage)
+  display(sectionEl)
+  arrayEl.textContent = array
+}
 
 const init = function () {
   display(mainEl)
@@ -68,3 +97,34 @@ const init = function () {
 
 // INITIALZE app
 init()
+
+//  SORTING ALGORITHMS
+var insertionSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Insertion Sort is being done.")
+}
+
+var selectionSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Selection Sort is being done.")
+}
+
+var mergeSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Merge Sort is being done.")
+}
+
+var quickSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Quick Sort is being done.")
+}
+
+var bubbleSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Bubble Sort is being done.")
+}
+
+var heapSort = function (sortingSpeedInMS) {
+  console.log(sortingSpeedInMS)
+  console.log("Heap Sort is being done.")
+}
